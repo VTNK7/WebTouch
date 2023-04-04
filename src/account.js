8776fabf538254a5ae3882e4be8ca986ba228e93
jere344 <jeremy.guerin34@yahoo.com>
2023-04-03 20:59:48 -0400

    First non functional outline

---
+++ src/account.js
@@ -0,0 +1,57 @@
+import Primus from 'primus';
+import HaapiConnection from './HaapiConnection.js';
+import Constants from './Constants.js';
+
+export default class Account {
+    haapi;
+    socket;
+    username;
+    password;
+    constants;
+
+
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+        this.constants = new Constants();
+    }
+
+    async start() {
+        await this.constants.setConfig();
+        if (!this.constants.config) {
+            console.log("Error in config loading ! (config is null)");
+            return;
+        }
+
+
+        this.haapi = new HaapiConnection({ account: this });
+        this.haapi.processHaapi(this.username, this.password);
+        this.connect(this.constants.config.sessionId, this.constants.config.dataUrl);
+    }
+
+    connect(sessionId, url) {
+        const currentUrl = this.makeSticky(url, sessionId);
+        console.log("Primus", "Connecting to login server (" + currentUrl + ") ...");
+        this.socket = this.createSocket(currentUrl);
+        this.socket.open();
+    }
+
+    makeSticky(url, sessionId) {
+        const seperator = url.indexOf("?") === -1 ? "?" : "&";
+        return url + seperator + "STICKER" + "=" + encodeURIComponent(sessionId);
+    }
+
+    createSocket(url) {
+        return new Primus(url, {
+            manual: true,
+            reconnect: {
+                max: Infinity,
+                min: 500,
+                retries: 10,
+            },
+            strategy: "disconnect,timeout",
+            transformer: "engine.io",
+        });
+    }
+
+}
---
+++ src/start.js
@@ -0,0 +1,15 @@
+import Account from './account.js';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+function start() {
+    if (!process.env.USERNAME || !process.env.PASSWORD) {
+        console.log('Please set USERNAME and PASSWORD environment variables. (.env file)');
+        process.exit(1);
+    }
+    var account = new Account(process.env.USERNAME, process.env.PASSWORD);
+    account.start();
+}
+
+start();
